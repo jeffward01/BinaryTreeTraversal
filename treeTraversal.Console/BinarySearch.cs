4a2b507db01baaff023c0e7a94c535d4c94f1b02
jeffward01 <jeffwardcoding@gmail.com>
2016-09-03 14:09:56 -0700

    Added binary Search Code

---
+++ treeTraversal.Console/BinarySearch.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace treeTraversal.Console
+{
+    public class BinarySearch
+    {
+        public static object BinarySearchIterative(int[] inputArray, int key, int min, int max)
+        {
+            while (min <= max)
+            {
+                int mid = (min + max)/2;
+                if (key == inputArray[mid])
+                {
+                    return ++mid;
+                }
+                else if (key < inputArray[mid])
+                {
+                    return max = mid - 1;
+                }
+                else if (key > inputArray[mid])
+                {
+                    return max = mid + 1;
+                }
+
+            }
+            return 'Nil';
+        }
+
+        public static object BinarySearchRecursive(int[] inputArray, int key, int min, int max)
+        {
+            if (min > max)
+            {
+                return "Nil";
+            }
+            else
+            {
+                int mid = (min + max)/2;
+                if (key == inputArray[mid])
+                {
+                    return ++mid;
+                }
+                else if(key < inputArray[mid])
+                {
+                    return BinarySearchRecursive(inputArray, key, min, max-1);
+                }
+                else if (key > inputArray[mid])
+                {
+                    return BinarySearchRecursive(inputArray, key, mid + 1, max);
+                }
+
+            }
+            return "Nil";
+        }
+    }
+}
