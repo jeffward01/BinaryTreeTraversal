270a8dd1b5ff988207f3db668039571b422f010b
jeffward01 <jeffwardcoding@gmail.com>
2016-09-03 19:39:13 -0700

    added binary search

--- treeTraversal.Console/BinarySearch.cs
+++ treeTraversal.Console/BinarySearch.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,7 +13,7 @@ namespace treeTraversal.Console
         {
             while (min <= max)
             {
-                int mid = (min + max)/2;
+                int mid = (min + max) / 2;
                 if (key == inputArray[mid])
                 {
                     return ++mid;
@@ -30,6 +31,8 @@ namespace treeTraversal.Console
             return 'Nil';
         }
 
+
+
         public static object BinarySearchRecursive(int[] inputArray, int key, int min, int max)
         {
             if (min > max)
@@ -38,14 +41,14 @@ namespace treeTraversal.Console
             }
             else
             {
-                int mid = (min + max)/2;
+                int mid = (min + max) / 2;
                 if (key == inputArray[mid])
                 {
                     return ++mid;
                 }
-                else if(key < inputArray[mid])
+                else if (key < inputArray[mid])
                 {
-                    return BinarySearchRecursive(inputArray, key, min, max-1);
+                    return BinarySearchRecursive(inputArray, key, min, max - 1);
                 }
                 else if (key > inputArray[mid])
                 {
@@ -55,5 +58,33 @@ namespace treeTraversal.Console
             }
             return "Nil";
         }
+
+
+        public static int simpleBinarySearch(int[] array, int value)
+        {
+            int low = 0;
+            int high = array.Length - 1;
+            int midpoint = 0;
+
+            while (low<=high)
+            {
+                midpoint = low + (high - low)/2;
+                if (value == array[midpoint])
+                {
+                    return midpoint;
+                }
+                else if(value < array[midpoint])
+                {
+                    high = midpoint - 1;
+                }
+                else
+                {
+                    low = midpoint + 1;
+                }
+            }
+            return -1;
+        }
     }
 }
+
+//http://www.c-sharpcorner.com/blogs/binary-search-implementation-using-c-sharp1
